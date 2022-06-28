be02f238109837707668eaba8c743584fa1244a9
Yohannes <yohatad000@gmail.com>
2022-06-28 15:12:03 +0300

    commit

---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+Hello and welcome to the second project of python in this directory we will find python if else as well as loops and function project files
---
+++ 0x02-python-import_modules/add_function.py
@@ -0,0 +1,7 @@
+def add(x,y,z):
+    sum = x + y + z;
+    return sum;
+num1 = int(input("Value of x:"))
+num2 = int(input("Value of y:"))
+num3 = int(input("Vaule of z:"))
+print("The sum of the three values are:", add(num1,num2,num3))
