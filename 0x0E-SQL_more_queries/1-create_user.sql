a335362277414ebbf0d3d9248d51351838b8d655
Yohannes <yohatad000@gmail.com>
2022-08-17 05:29:34 +0200

    changes

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- a script that creates the MySQL server user user_0d_1
+-- the two asterix represent table and data respectively
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- a script that lists all cities contained in the database 
+SELECT cities.id, cities.name, states.name
+FROM cities JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
